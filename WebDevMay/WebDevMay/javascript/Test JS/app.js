7b954d4ce3048c1c3f50b7768908885d73b77e1f
WinandVM <winand_vanmeerbeek@hotmail.com>
2022-06-10 18:43:41 +0200

    10/06 End of Day

--- WebDevMay/WebDevMay/javascript/Test JS/app.js
+++ WebDevMay/WebDevMay/javascript/Test JS/app.js
@@ -1,4 +1,9 @@
-const getValueInput = () =>{
-    let inputValue = document.getElementById("temperature").value; 
-    document.getElementById("valueInput").innerText = inputValue >=20 ? "My advice: don't wear a coat":"My advice: wear a coat"; 
-  }
+// const getValueInput = () =>{
+//     let inputValue = document.getElementById("temperature").value; 
+//     document.getElementById("valueInput").innerText = inputValue >=20 ? "My advice: don't wear a coat":"My advice: wear a coat"; 
+//   }
+
+  function getValueInput() {
+  let inputValue = document.getElementById("temperature").value;
+  document.getElementById("valueInput").innerText = inputValue >= 20 ? "My advice: don't wear a coat" : "My advice: wear a coat";
+}
